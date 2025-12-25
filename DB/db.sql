0e6e78bd27c8d51212531ab8fffa9e2b76dfd825
AthavanDS <athavands4@gmail.com>
2025-12-25 08:54:46 +0530

    db updated

---
+++ DB/db.sql
@@ -0,0 +1,125 @@
+/*
+SQLyog Ultimate v13.1.1 (64 bit)
+MySQL - 8.3.0 : Database - user_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `categories` */
+
+/*Table structure for table `crime_files` */
+
+DROP TABLE IF EXISTS `crime_files`;
+
+CREATE TABLE `crime_files` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `subject_number` varchar(50) NOT NULL,
+  `division` varchar(100) NOT NULL,
+  `police_station` varchar(100) NOT NULL,
+  `crime` varchar(100) NOT NULL,
+  `in_date` date NOT NULL,
+  `court_number` varchar(150) DEFAULT NULL,
+  `gcr_number` varchar(50) DEFAULT NULL,
+  `in_word_no_date` varchar(100) DEFAULT NULL,
+  `division_station_out_word_date` varchar(100) DEFAULT NULL,
+  `remember_date` date DEFAULT NULL,
+  `dir_legal_out_word_date` varchar(100) DEFAULT NULL,
+  `dir_legal_subject_number` varchar(100) DEFAULT NULL,
+  `category_id` int NOT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  `deleted_by` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `crime_files` */
+
+/*Table structure for table `pdf_stuff` */
+
+DROP TABLE IF EXISTS `pdf_stuff`;
+
+CREATE TABLE `pdf_stuff` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `crime_file_id` int NOT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `category_id` int NOT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  `deleted_by` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `crime_file_id` (`crime_file_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `pdf_stuff` */
+
+/*Table structure for table `seen_info` */
+
+DROP TABLE IF EXISTS `seen_info`;
+
+CREATE TABLE `seen_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pdf_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `pdf_id` (`pdf_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `seen_info` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_pic` varchar(255) DEFAULT 'default.png',
+  `role` enum('super_admin','user') DEFAULT 'user',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `category_id` int DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `fk_users_category` (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`profile_pic`,`role`,`created_at`,`category_id`,`created_by`,`deleted_at`) values 
+(1,'admin','21232f297a57a5a743894a0e4a801fc3','1766430253_548525649_782641601600847_6336705901751037841_n.jpg','super_admin','2025-12-23 00:31:38',2,NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
